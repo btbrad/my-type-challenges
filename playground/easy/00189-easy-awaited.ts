8a3fe035c05dffe382679e9e8ec0b0c49d46af76
btbrad <biantaobrad@gmail.com>
2025-04-12 15:25:57 +0800

    提取Promise类型

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,7 +22,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends PromiseLike<infer U> ? MyAwaited<U> : T
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
