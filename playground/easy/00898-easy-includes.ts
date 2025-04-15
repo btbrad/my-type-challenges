3c274bbc0460c6636a436dc869be9c52c7a30839
btbrad <biantaobrad@gmail.com>
2025-04-15 16:40:42 +0800

    实现数组includes

--- playground/easy/00898-easy-includes.ts
+++ playground/easy/00898-easy-includes.ts
@@ -17,8 +17,10 @@
 */
 
 /* _____________ 你的代码 _____________ */
+type IsEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2)
+  ? true : false
 
-type Includes<T extends readonly any[], U> = any
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ? IsEqual<First, U> extends true ? true : Includes<Rest, U> : false
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
