ed37d07783278426f324971e51ab499950061d4f
btbrad <biantaobrad@gmail.com>
2025-04-23 16:21:32 +0800

    对象部分属性只读

--- playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
+++ playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
@@ -34,7 +34,11 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyReadonly2<T, K> = any
+type MyReadonly2<T extends object, K extends keyof T = keyof T> = {
+  +readonly [Key in keyof T as Key extends K ? Key : never]: T[Key]
+} & {
+  [Key in keyof T as Key extends Exclude<keyof T, K> ? Key : never]: T[Key]
+}
 
 /* _____________ 测试用例 _____________ */
 import type { Alike, Expect } from '@type-challenges/utils'
