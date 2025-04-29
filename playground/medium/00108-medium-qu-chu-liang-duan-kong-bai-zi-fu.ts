f716533ff10194bddddf70ff85f0680a47f4caa9
btbrad <biantaobrad@gmail.com>
2025-04-29 17:15:24 +0800

    去除字符串两端空白

--- playground/medium/00108-medium-qu-chu-liang-duan-kong-bai-zi-fu.ts
+++ playground/medium/00108-medium-qu-chu-liang-duan-kong-bai-zi-fu.ts
@@ -18,7 +18,8 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Trim<S extends string> = any
+type Space = ' ' | '\n' | '\t'
+type Trim<S extends string> = S extends `${Space}${infer R}` | `${infer R}${Space}` ? Trim<R> : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
