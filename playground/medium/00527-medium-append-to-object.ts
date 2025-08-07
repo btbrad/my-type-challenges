254166a517c3c91b879f21652427fcdcb94fe44e
btbrad <biantaobrad@gmail.com>
2025-08-07 16:39:45 +0800

    为接口添加新的字段

--- playground/medium/00527-medium-append-to-object.ts
+++ playground/medium/00527-medium-append-to-object.ts
@@ -19,7 +19,9 @@
 
 /* _____________ 你的代码 _____________ */
 
-type AppendToObject<T, U, V> = any
+type AppendToObject<T, U extends keyof any, V> = {
+  [Key in keyof T | U]: Key extends keyof T ? T[Key] : V
+}
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
